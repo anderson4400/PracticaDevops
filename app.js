af3f47780da1003ec1dd4aca97439e930b7b906f
= <=>
2021-11-02 11:30:18 -0400

    PrimerCommit

---
+++ app.js
@@ -0,0 +1,19 @@
+// let nombre = "jaun";
+// let apellido = "navares";
+
+// console.log(nombre);
+// console.log(apellido);
+
+var http = require('http');
+var server = http.createServer();
+ 
+function mensaje(petic, resp) {
+	resp.writeHead(200, {'content-type': 'text/plain'});
+	resp.write('Hola Mundo');
+	resp.end();
+}
+server.on('request', mensaje);
+ 
+server.listen(3000, function () {
+  	console.log('La Aplicación está funcionando en el puerto 3000');
+});
