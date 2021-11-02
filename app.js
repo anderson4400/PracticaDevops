bb03144b5a96bec1ce30eb82ed1ea11bb836f6f5
anderson4400 <42215687+anderson4400@users.noreply.github.com>
2021-11-02 19:46:29 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,19 +1,8 @@
-// let nombre = "jaun";
-// let apellido = "navares";
+const  express = require('express');
+const  app = express()
 
-// console.log(nombre);
-// console.log(apellido);
+app.get('/',(req, res) =>res.send('Hello DevOps'));
 
-var http = require('http');
-var server = http.createServer();
- 
-function mensaje(petic, resp) {
-	resp.writeHead(200, {'content-type': 'text/plain'});
-	resp.write('Hola Mundo');
-	resp.end();
-}
-server.on('request', mensaje);
- 
-server.listen(3000, function () {
-  	console.log('La Aplicación está funcionando en el puerto 3000');
-});
+app.listen(3000, () =>{
+    console.log('My RES API running on port 3000!');
+})
